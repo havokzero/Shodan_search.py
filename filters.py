15cb152c0a8114142b929128db873ec88116fe5b
havokzero <46027877+havokzero@users.noreply.github.com>
2024-05-17 15:20:42 -0700

    Create filters.py

---
+++ filters.py
@@ -0,0 +1,17 @@
+FILTERS = {
+    "SSL": [
+        "ssl.cert.alg", "ssl.cert.expired", "ssl.cert.extension", "ssl.cert.fingerprint",
+        "ssl.cert.issuer.cn", "ssl.cert.pubkey.bits", "ssl.cert.pubkey.type",
+        "ssl.cert.serial", "ssl.cert.subject.cn", "ssl.chain_count", "ssl.cipher.bits",
+        "ssl.cipher.name", "ssl.cipher.version", "ssl.ja3s", "ssl.jarm", "ssl.version"
+    ],
+    "NTP": [
+        "ntp.ip", "ntp.ip_count", "ntp.more", "ntp.port"
+    ],
+    "Telnet": [
+        "telnet.do", "telnet.dont", "telnet.option", "telnet.will", "telnet.wont"
+    ],
+    "SSH": [
+        "ssh.hassh", "ssh.type"
+    ]
+}
